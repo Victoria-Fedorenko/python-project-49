3cf53c662ae08e028b66f4886b4ae09954a070d3
creative-projects-gm <vnfedorenko97@yandex.ru>
2025-07-12 23:14:29 +0300

    make lint command added, errors are fixed

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,17 @@
-def brain_games():
-    print('Welcome to the Brain Games!')
-
 from ..cli import welcome_user
 
+
+def brain_games():
+    
+    print('Welcome to the Brain Games!')
+
+
 def main():
+    
     brain_games()
     welcome_user()
+    
 
 if __name__ == "__main__":
+    
     main()
