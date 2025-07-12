5d5a940ba4435c625a33007814575460de932052
creative-projects-gm <vnfedorenko97@yandex.ru>
2025-07-12 22:16:02 +0300

    created module welcome_user in cli.py; welcome_user added in brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,11 @@
 def brain_games():
     print('Welcome to the Brain Games!')
 
+from ..cli import welcome_user
+
 def main():
     brain_games()
+    welcome_user()
 
 if __name__ == "__main__":
     main()
