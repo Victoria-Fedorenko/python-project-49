c94e9ce957c3ec2fac2681675fbbe54664fcab86
creative-projects-gm <vnfedorenko97@yandex.ru>
2025-07-18 19:19:49 +0300

    add new game brain-gcd and brain_gcd.py

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,36 @@
+import prompt
+import random
+from brain_games.functions import say_hi_and_get_name, check_answer, game_cycle
+
+
+def get_random_numbers_and_correct_gcd():
+    a = random.randint(0, 100)
+    b = random.randint(0, 100)
+    random_numbers = f'{a} {b}'
+    if a == 0:
+        correct_answer = b
+    elif b == 0:
+        correct_answer = a
+    else:
+        while b > 0: 
+            o = a%b
+            a = b
+            b = o
+        correct_answer = a
+    return random_numbers, str(correct_answer)
+    
+
+def gcd_game():
+
+    name = say_hi_and_get_name()
+    print('Find the greatest common divisor of given numbers.')
+    game_cycle(check_answer, name, get_random_numbers_and_correct_gcd)
+
+
+def main():
+
+    gcd_game()
+
+if __name__ == "__main__":
+    
+    main()
