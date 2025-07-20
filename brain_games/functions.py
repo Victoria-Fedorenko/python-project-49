1fdab1136e1d7ce42af83a2d1ed9d5ff55d594ff
Victoria-Fedorenko <vnfedorenko97@gmail.com>
2025-07-20 11:29:56 +0300

    linter error fixed

--- brain_games/functions.py
+++ brain_games/functions.py
@@ -29,7 +29,10 @@ def game_cycle(check_answer, name, function):
             if i == 2:
                 print(f'Congratulations, {name}!')
         elif check_result is False:
-            print(f'{user_answer} is wrong answer ;(. Correct answer was {correct_answer}.')
+            print(
+                f'{user_answer} is wrong answer ;(. '
+                f'Correct answer was {correct_answer}.'
+            )
             print(f"Let's try again, {name}!")
             break
 
